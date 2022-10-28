7bd4158abe627d10ad9d6e23220f8f010e7ddbdb
Mostafa Bayomi <moustafa.m.bayoumi@gmail.com>
2022-10-28 17:25:59 +0300

    add validation for mongoose id

--- middleware/validate.js
+++ middleware/validate.js
@@ -1,7 +1,14 @@
+import mongoose from "mongoose";
+
 export default (validator) => {
   return (req, res, next) => {
     const { error } = validator(req.body);
     if (error) return res.status(422).send(error.details[0].message);
+
+    if (req.params) {
+      if (!mongoose.Types.ObjectId.isValid(req.params.id))
+        return res.status(404).send("Invalid ID.");
+    }
     next();
   };
 };
