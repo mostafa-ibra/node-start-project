7db0370bb36e9bdafa9c65796b7113ff01742186
Mostafa Bayomi <moustafa.m.bayoumi@gmail.com>
2022-10-28 17:00:44 +0300

    modify status of validate middleware

--- middleware/validate.js
+++ middleware/validate.js
@@ -1,8 +1,7 @@
 export default (validator) => {
   return (req, res, next) => {
     const { error } = validator(req.body);
-    if (error)
-      return res.status(400).send(error.details[0].message);
+    if (error) return res.status(422).send(error.details[0].message);
     next();
   };
 };
